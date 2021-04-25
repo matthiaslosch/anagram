3f44d1e3eb21f03e3a47a205bae15695ed056e0b
Matthias Losch <losch@hey.com>
2021-04-25 16:31:56 +0200

    Ignore letter case

--- main.c
+++ main.c
@@ -14,11 +14,19 @@ int main(int argc, char **argv)
     int factor1 = 1;
     int factor2 = 1;
 
-    for (int i = 0; argv[1][i]; ++i)
-        factor1 *= primes[argv[1][i] - 97];
+    for (int i = 0; argv[1][i]; ++i) {
+        if (argv[1][i] > 90)
+            factor1 *= primes[argv[1][i] - 97];
+        else
+            factor1 *= primes[argv[1][i] - 65];
+    }
 
-    for (int i = 0; argv[2][i]; ++i)
-        factor2 *= primes[argv[2][i] - 97];
+    for (int i = 0; argv[2][i]; ++i) {
+        if (argv[2][i] > 90)
+            factor2 *= primes[argv[2][i] - 97];
+        else
+            factor2 *= primes[argv[2][i] - 65];
+    }
 
     if (factor1 == factor2)
         printf("true\n");
